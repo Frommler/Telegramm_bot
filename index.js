f99d7bfc4eac03d3dc09534a16ef6c1cc03fdc5b
Alex Vakula <albanec777@meta.ua>
2022-09-25 12:22:28 +0200

    working version 1,0

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const Telegraf = require("telegraf").Telegraf,
 
 const Bot = new Telegraf(TOKEN);
 
+let dataFromServer = [];
+let dateOfServer = "";
+
 Bot.start((ctx) => {
-  return ctx.reply("Welcome");
-});
-
-Bot.hears(/hi\W$/i, (ctx) => {
-  return ctx.reply("\u{1f609}");
-});
-
-Bot.hears(/[A-Z]+/i, (ctx) => {
-  console.log(ctx.message.text);
   fetch("https://russianwarship.rip/api/v1/statistics/latest", {
     method: "get",
     headers: { "Content-Type": "application/json" },
@@ -21,9 +15,41 @@ Bot.hears(/[A-Z]+/i, (ctx) => {
       return res.json();
     })
     .then((data) => {
-      return ctx.reply(data.data.stats[ctx.message.text.toLowerCase()]);
+      // return ctx.reply(data.data.stats[ctx.message.text.toLowerCase()]);
+      dateOfServer = data.data.date;
+      var date = new Date();
+      var current_date =
+        date.getFullYear() + "-" + (("0"+ (date.getMonth() + 1)).slice(-2)) + "-" + date.getDate();
+      dataFromServer = data.data.stats;
+      console.log(dateOfServer);
+      console.log(current_date);
     })
     .catch((err) => ctx.reply("What do you mean?"));
+  return ctx.reply("Welcome");
+});
+
+Bot.hears(/hi+/i, (ctx) => {
+  console.log("Hello!");
+  ctx.reply("Hello!");
+});
+
+Bot.hears(/[А-Я]+/i, (ctx) => {
+  let key = "";
+  console.log(ctx.message.text);
+  switch (ctx.message.text.toLowerCase()) {
+    case "літаки":
+      key = "planes";
+      break;
+    case "танки":
+      key = "tanks";
+      break;
+    case "люди":
+      key = "personnel_units";
+      break;
+    default:
+      return ctx.reply("Sorry. I have no these word");
+  }
+  ctx.reply(dataFromServer[key]);
 });
 
 Bot.launch();
